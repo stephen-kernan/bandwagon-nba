b71e173f1fb10e5991c960c938548a017066ab5a
stephen-kernan <skernan@harding.edu>
2019-06-17 06:19:04 -0500

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    bandwagonnba.com
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
