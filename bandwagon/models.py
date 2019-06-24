f248ca9aa65e16f48a5007afe804faf9107fd7fc
stephen-kernan <skernan@harding.edu>
2019-06-23 19:05:34 -0500

    Adjusted Design and Added Image in Posts

--- bandwagon/models.py
+++ bandwagon/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='post_pics')
 
     def __str__(self):
         return self.title
