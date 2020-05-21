22ff9501d975feb8866555c8e82cc1bbe7ff396d
Stephen Kernan <skernan@harding.edu>
2020-05-21 07:31:16 -0500

    Updated DEBUG to false for production

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
     'bandwagonnba.herokuapp.com'
